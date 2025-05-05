ae25e7bbf9287240782d3ffdfc7e7fc33db8a42e
Riccardo Zampieri <riccardo.zampieri28@gmail.com>
2025-05-05 21:59:33 +0200

    Refactored code

--- internal/report/report.go
+++ internal/report/report.go
@@ -8,98 +8,68 @@ import (
 	"github.com/zampitek/filecheck/internal/scanner"
 )
 
-var ageLow []scanner.FileInfo
-var ageMedium []scanner.FileInfo
-var ageHigh []scanner.FileInfo
-var Top5Oldest []scanner.FileInfo
-
-var sizeLow []scanner.FileInfo
-var sizeMedium []scanner.FileInfo
-var sizeHigh []scanner.FileInfo
-var Top5Heaviest []scanner.FileInfo
-
 type Report struct {
 	Modified90   []scanner.FileInfo
 	Modified180  []scanner.FileInfo
 	ModifiedHigh []scanner.FileInfo
-
-	Size10   []scanner.FileInfo
-	Size100  []scanner.FileInfo
-	sizeHigh []scanner.FileInfo
-}
-
-type ExtendedReport struct {
-	Modified90   []scanner.FileInfo
-	Modified180  []scanner.FileInfo
-	ModifiedHigh []scanner.FileInfo
-	Top5Oldest   []scanner.FileInfo
-
 	Size10       []scanner.FileInfo
 	Size100      []scanner.FileInfo
 	sizeHigh     []scanner.FileInfo
+}
+
+type ExtendedReport struct {
+	Report
+	Top5Oldest   []scanner.FileInfo
 	Top5Heaviest []scanner.FileInfo
 }
 
-func cathegorizeFilesByAge(file scanner.FileInfo) {
-	if file.LastAccess < 30 {
-		return
-	} else if file.LastAccess < 90 {
-		ageLow = append(ageLow, file)
-	} else if file.LastAccess < 180 {
-		ageMedium = append(ageMedium, file)
-	} else {
-		ageHigh = append(ageHigh, file)
-	}
-}
+func cathegorizeFiles(files []scanner.FileInfo) (age90, age180, ageHigh, size50, size500, sizeHigh []scanner.FileInfo) {
+	for _, file := range files {
+		if !file.IsDir {
+			switch {
+			case file.Size < 50*1024*1024:
+				size50 = append(size50, file)
+			case file.Size < 500*1024*1024:
+				size500 = append(size500, file)
+			default:
+				sizeHigh = append(sizeHigh, file)
+			}
+		}
 
-func cathegorizeFilesBySize(file scanner.FileInfo) {
-	if file.IsDir {
-		return
+		switch {
+		case file.LastAccess < 90:
+			age90 = append(age90, file)
+		case file.LastAccess < 180:
+			age180 = append(age180, file)
+		default:
+			ageHigh = append(ageHigh, file)
+		}
 	}
 
-	if file.Size < 50*1024*1024 {
-		sizeLow = append(sizeLow, file)
-	} else if file.Size < 500*1024*1024 {
-		sizeMedium = append(sizeMedium, file)
-	} else {
-		sizeHigh = append(sizeHigh, file)
-	}
+	return
 }
 
 func CreateReport(report scanner.ScanReport) Report {
-	for _, f := range report.Files {
-		cathegorizeFilesByAge(f)
-		cathegorizeFilesBySize(f)
-	}
-
-	return Report{ageLow, ageMedium, ageHigh,
-		sizeLow, sizeMedium, sizeHigh}
+	age90, age180, ageHigh, size50, size500, sizeHigh := cathegorizeFiles(report.Files)
+	return Report{age90, age180, ageHigh, size50, size500, sizeHigh}
 }
 
 func CreateExtendedReport(report scanner.ScanReport) ExtendedReport {
-	for _, f := range report.Files {
-		cathegorizeFilesByAge(f)
-		cathegorizeFilesBySize(f)
-	}
+	r := CreateReport(report)
 
-	sort.Slice(ageHigh, func(i, j int) bool {
-		return ageHigh[i].LastAccess > ageHigh[j].LastAccess
+	sort.Slice(r.ModifiedHigh, func(i, j int) bool {
+		return r.ModifiedHigh[i].LastAccess > r.ModifiedHigh[j].LastAccess
 	})
+	topAge := min(len(r.ModifiedHigh), 5)
+	topOldest := r.ModifiedHigh[:topAge]
 
-	numFilesAge := len(ageHigh)
-	countAge := min(numFilesAge, 5)
-	Top5Oldest = ageHigh[:countAge]
-
-	sort.Slice(sizeHigh, func(i, j int) bool {
-		return sizeHigh[i].Size > sizeHigh[j].Size
+	sort.Slice(r.sizeHigh, func(i, j int) bool {
+		return r.sizeHigh[i].Size > r.sizeHigh[j].Size
 	})
+	topSize := min(len(r.sizeHigh), 5)
+	topHeaviest := r.sizeHigh[:topSize]
 
-	numFilesSize := len(sizeHigh)
-	countSize := min(numFilesSize, 5)
-	Top5Heaviest = sizeHigh[:countSize]
-
-	return ExtendedReport{ageLow, ageMedium, ageHigh, Top5Oldest,
-		sizeLow, sizeMedium, sizeHigh, Top5Heaviest}
+	return ExtendedReport{Report: r, Top5Oldest: topOldest, Top5Heaviest: topHeaviest}
 }
 
 func PrintReport(report Report) string {
@@ -114,6 +84,7 @@ func PrintReport(report Report) string {
 
 		problems = true
 	}
+
 	if len(report.Size10) != 0 || len(report.Size100) != 0 || len(report.sizeHigh) != 0 {
 		builder.WriteString("\nFound several file with a considerable size:\n\n")
 		builder.WriteString(fmt.Sprintf("\tLOW SEVERITY (size less than 50 MB): %d files\n", len(report.Size10)))
@@ -131,36 +102,22 @@ func PrintReport(report Report) string {
 }
 
 func PrintExtendedReport(report ExtendedReport) string {
+	output := PrintReport(report.Report)
 	var builder strings.Builder
-	var problems = false
+	builder.WriteString(output)
 
-	if len(report.Modified90) != 0 || len(report.Modified180) != 0 || len(report.ModifiedHigh) != 0 {
-		builder.WriteString("Found several file modified over 30 days ago:\n\n")
-		builder.WriteString(fmt.Sprintf("\tLOW SEVERITY (modified within the last 90 days): %d files\n", len(report.Modified90)))
-		builder.WriteString(fmt.Sprintf("\tMEDIUM SEVERITY (modified within the last 180 days): %d files\n", len(report.Modified180)))
-		builder.WriteString(fmt.Sprintf("\tHIGH SEVERITY (modified over 180 days ago): %d files\n\n", len(report.ModifiedHigh)))
-
+	if len(report.Top5Oldest) > 0 {
+		builder.WriteString("\nTop 5 oldest files:\n")
 		for i, file := range report.Top5Oldest {
-			builder.WriteString(fmt.Sprintf("\t%d. %s\t(%d days)\n", i, file.Path, file.LastAccess))
+			builder.WriteString(fmt.Sprintf("\t%d. %s\t (%d days)\n", i+1, file.Path, file.LastAccess))
 		}
-
-		problems = true
 	}
-	if len(report.Size10) != 0 || len(report.Size100) != 0 || len(report.sizeHigh) != 0 {
-		builder.WriteString("\nFound several file with a considerable size:\n\n")
-		builder.WriteString(fmt.Sprintf("\tLOW SEVERITY (size less than 50 MB): %d files\n", len(report.Size10)))
-		builder.WriteString(fmt.Sprintf("\tMEDIUM SEVERITY (size less than 500 MB): %d files\n", len(report.Size100)))
-		builder.WriteString(fmt.Sprintf("\tHIGH SEVERITY (size over 500 MB): %d files\n\n", len(report.sizeHigh)))
 
+	if len(report.Top5Heaviest) > 0 {
+		builder.WriteString("\nTop 5 heaviest files:\n")
 		for i, file := range report.Top5Heaviest {
-			builder.WriteString(fmt.Sprintf("\t%d. %s\t(%d MB)\n", i, file.Path, file.Size/1024/1024))
+			builder.WriteString(fmt.Sprintf("\t%d. %s\t (%d MB)\n", i+1, file.Path, file.Size/1024/1024))
 		}
-
-		problems = true
-	}
-
-	if !problems {
-		builder.WriteString("No problems found\n")
 	}
 
 	return builder.String()
